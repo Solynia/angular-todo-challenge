ae8927e3d38e00d627eabef0575f1605f89d4679
Sebastien Bouvier <sebastien.bouvier@joorney.com>
2025-03-11 17:34:57 +0700

    feat: update state with new todo name

--- src/app/modules/todo/components/edit-todo-dialog/edit-todo-dialog.component.spec.ts
+++ src/app/modules/todo/components/edit-todo-dialog/edit-todo-dialog.component.spec.ts
@@ -1,15 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatDialogRef } from '@angular/material/dialog';
-import { EditTodoDialogComponent } from './edit-todo-dialog.component';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import {
+  EditTodoDialogComponent,
+  EditTodoDialogData,
+} from './edit-todo-dialog.component';
 
 describe('EditTodoDialogComponent', () => {
   let component: EditTodoDialogComponent;
   let fixture: ComponentFixture<EditTodoDialogComponent>;
+  const todo = { name: 'My first todo' };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [EditTodoDialogComponent],
-      providers: [{ provide: MatDialogRef, useValue: {} }],
+      providers: [
+        {
+          provide: MatDialogRef,
+          useValue: { close: (todo?: { name: string }) => {} },
+        },
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: { todo } as EditTodoDialogData,
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(EditTodoDialogComponent);
@@ -19,5 +32,52 @@ describe('EditTodoDialogComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should submit the valid todo name', () => {
+    const submitSpy = jest.spyOn(component, 'onSubmit');
+    const compiled = fixture.nativeElement as HTMLElement;
+    const button =
+      compiled.querySelector<HTMLButtonElement>('button#submitBtn')!;
+    const input = compiled.querySelector('input')!;
+    const newName = `${todo.name} - edited`;
+    input.value = newName;
+
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      button.click();
+
+      expect(submitSpy).toHaveBeenCalled();
+      expect(component.newName).toBe(newName);
+    });
+  });
+
+  it('should not submit an invalid todo name', () => {
+    const submitSpy = jest.spyOn(component, 'onSubmit');
+    const compiled = fixture.nativeElement as HTMLElement;
+    const button =
+      compiled.querySelector<HTMLButtonElement>('button#submitBtn')!;
+    const input = compiled.querySelector('input')!;
+    const newName = '';
+    input.value = newName;
+
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      button.click();
+
+      expect(submitSpy).not.toHaveBeenCalled();
+      expect(component.newName).toBe(newName);
+    });
+  });
+
+  it('should cancel the edit', () => {
+    const cancelSpy = jest.spyOn(component, 'onCancel');
+    const compiled = fixture.nativeElement as HTMLElement;
+    const button =
+      compiled.querySelector<HTMLButtonElement>('button#cancelBtn')!;
+
+    button.click();
+
+    expect(cancelSpy).toHaveBeenCalled();
   });
 });
--- src/app/modules/todo/components/edit-todo-dialog/edit-todo-dialog.component.ts
+++ src/app/modules/todo/components/edit-todo-dialog/edit-todo-dialog.component.ts
@@ -2,7 +2,7 @@ import { CdkScrollable } from '@angular/cdk/scrolling';
 import { Component, OnInit, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
-import { MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogActions, MatDialogContent, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { Todo } from 'src/app/modules/store/todo/todo.reducer';
@@ -15,18 +15,23 @@ export interface EditTodoDialogData {
     selector: 'app-edit-todo-dialog',
     templateUrl: './edit-todo-dialog.component.html',
     styleUrls: ['./edit-todo-dialog.component.css'],
-    imports: [MatDialogTitle, CdkScrollable, MatDialogContent, MatFormField, MatLabel, MatInput, FormsModule, MatDialogActions, MatButton, MatDialogClose]
+    imports: [MatDialogTitle, CdkScrollable, MatDialogContent, MatFormField, MatLabel, MatInput, FormsModule, MatDialogActions, MatButton]
 })
 export class EditTodoDialogComponent implements OnInit {
-  private readonly dialogRef = inject<MatDialogRef<EditTodoDialogComponent>>(MatDialogRef);
+  private readonly dialogRef = inject<MatDialogRef<EditTodoDialogComponent, Todo>>(MatDialogRef);
+  readonly data = inject<EditTodoDialogData>(MAT_DIALOG_DATA);
 
   newName = '';
 
   ngOnInit(): void {
+    this.newName = this.data.todo.name ?? '';
   }
 
   onCancel(): void {
     this.dialogRef.close();
   }
 
+  onSubmit(): void {
+    this.dialogRef.close({ ...this.data.todo, name: this.newName });
+  }
 }
